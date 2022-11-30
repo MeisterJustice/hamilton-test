6bbe7eab9e928a62b205a58d4d27c8f6d26ea413
meisterjustice <eziefulejustice@gmail.com>
2022-11-30 17:49:56 +0100

    chore: handle search

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import classNames from "classnames";
@@ -6,6 +6,7 @@ import classNames from "classnames";
 import { getCharacters } from "../application/actions/character";
 import { getCharacters as characters } from "../application/selectors/character";
 import { Container, Pagination } from "../components/Ui";
+import { debounce } from "../libs/utils";
 
 const HomePage = () => {
   const dispatch = useDispatch();
@@ -14,7 +15,7 @@ const HomePage = () => {
 
   const [page, setPage] = useState<number>(1);
   const [totalPages, setTotalPages] = useState<number>(1);
-  console.log(totalPages, results);
+  const [searchText, setSearchText] = useState<string>("");
 
   const pageArray = Array.from(Array(totalPages), (_, x) => x + 1);
 
@@ -24,10 +25,6 @@ const HomePage = () => {
       page === totalPages ? 0 : page - 1,
       page === totalPages ? 2 : page + 2
     );
-
-  const callGetCharacters = (query?: string) => {
-    dispatch(getCharacters(query || ""));
-  };
 
   const handleNumberToSkip = (type: string | number) => {
     switch (type) {
@@ -51,6 +48,12 @@ const HomePage = () => {
     }
   };
 
+  const callGetCharacters = (query?: string) => {
+    // const searchQuery = searchText ? `&nameStartsWith=${searchText}` : "";
+    // const allQuery = (query || "") + searchQuery;
+    dispatch(getCharacters(query || ""));
+  };
+
   const handlePagination = (type: string | number) => {
     const skip = handleNumberToSkip(type);
     const query = `&offset=${skip}`;
@@ -63,6 +66,13 @@ const HomePage = () => {
   const handleCustomPagination = (page: number) => {
     return (page - 1) * limit!;
   };
+
+  const handleSearchText = debounce((value: string) => {
+    setSearchText(value);
+    const query = value ? `&nameStartsWith=${value}` : "";
+    callGetCharacters(query);
+    setPage(1);
+  }, 10);
 
   useEffect(() => {
     callGetCharacters();
@@ -82,6 +92,14 @@ const HomePage = () => {
         <div className="py-10 xl:py-20">
           <h1 className="lg:text-h1 text-h1-sm">Marvel Characters</h1>
           <div className="mt-10">
+            <div className="my-5 lg:flex justify-between items-center">
+              <input
+                value={searchText}
+                onChange={(e) => handleSearchText(e.target.value)}
+                placeholder="search"
+                className="outline-gray-500 border border-gray-600 rounded h-12 w-full lg:w-80 p-3"
+              />
+            </div>
             <table className="table-auto w-full overflow-x-auto border">
               <thead className="border rounded">
                 <tr className="p-8">
--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,3 +1,14 @@
 export const isProduction = () => {
   return process.env.NODE_ENV === "production";
 };
+
+export const debounce = (callback: Function, wait: number = 500) => {
+  let timer: any = undefined;
+
+  return (...args: any) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      callback.apply(this, args);
+    }, wait);
+  };
+};
